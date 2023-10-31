4cebb71cafb5ae0c6dced6fdf80bdd403ccb1ff0
Felipe Benoit <benoit@Felipes-MacBook-Pro.local>
2023-10-30 21:06:08 -0400

    added const questioons 1

---
+++ script.js
@@ -0,0 +1,8 @@
+const questions = [
+    {
+        question: "What is JavaScript?",
+        options: ["A programming language", "A fruit", "A car"],
+        answer: 0
+    },
+    // Add more questions here
+];
